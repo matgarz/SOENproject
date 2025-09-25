6d7b36a2c483a36879b5db6115ce2ef793a5335d
karimikhaeil <135124371+karimikhaeil@users.noreply.github.com>
2025-09-25 19:17:10 -0400

    Testing commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import './App.css'
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -43,7 +43,7 @@ function App() {
       </p>
 
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more, Testing to see
       </p>
     </>
   );
