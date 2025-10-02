2583e2b4d4f1de3cea57e22f563dec49fbd6bb84
Jasdeep <jasdeep.singh@spaceconcordia.ca>
2025-10-02 14:20:03 -0400

    Update JSON

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,13 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState(""); // state for backend data
-
-  // Fetch from backend
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("Error fetching:", err));
-  }, []);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>SOEN 341 Project</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      {/* Backend message */}
-      <p style={{ color: "green", fontWeight: "bold" }}>
-        {message || "Loading backend message..."}
-      </p>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <main className='text-3xl text-indigo-400 font-bold underline'>
+      <h1>
+      SOEN 341 Project - National
+      </h1>
+    </main>
+  )
 }
 
-export default App;
+export default App
